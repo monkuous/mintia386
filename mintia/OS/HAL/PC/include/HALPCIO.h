35428b3d5449614cdfa538e30bdf3e9af06ddf96
monkuous <84319635+monkuous@users.noreply.github.com>
2025-12-19 18:32:20 +0100

    HAL/PC: implement PIC driver

---
+++ mintia/OS/HAL/PC/include/HALPCIO.h
@@ -0,0 +1,11 @@
+extern HALPCInb { port -- byte }
+
+extern HALPCIni { port -- int }
+
+extern HALPCInl { port -- long }
+
+extern HALPCOutb { byte port -- }
+
+extern HALPCOuti { int port -- }
+
+extern HALPCOutl { long port -- }
